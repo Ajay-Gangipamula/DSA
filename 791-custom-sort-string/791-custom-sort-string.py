cd9489252a2737ef38a7a19a210f3e7a36b75c79
GANGIPAMULA VENKATA SURYA AJAY <43990079+Ajay-Gangipamula@users.noreply.github.com>
2022-09-15 18:14:13 +0530

    Time: 84 ms (5.25%), Space: 14 MB (18.10%) - LeetHub

---
+++ 791-custom-sort-string/791-custom-sort-string.py
@@ -0,0 +1,22 @@
+from functools import cmp_to_key
+def compare(t1:Tuple,t2:Tuple)->int:
+    if(t1[0]<t2[0]):
+        return -1
+    if(t1[0] > t2[0]):
+        return 1
+    return 0
+
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        rank=[100000]*26
+        for i in range(len(order)):
+            rank[ord(order[i])-ord('a')]=i
+        #rint(rank)
+        l=[]
+        for i in range(len(s)):
+            l.append((rank[ord(s[i])-ord('a')],s[i]))
+        l.sort(key=cmp_to_key(compare))
+        s1=''
+        for i in range(len(l)):
+            s1+=l[i][1]
+        return s1
