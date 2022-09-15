d76fe883b724a514ae1476bae100cfaee62ba544
GANGIPAMULA VENKATA SURYA AJAY <43990079+Ajay-Gangipamula@users.noreply.github.com>
2022-09-15 10:46:44 +0530

    Time: 1228 ms (53.79%), Space: 20.2 MB (81.37%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -1,5 +1,5 @@
 from functools import cmp_to_key
-def compare(k1:list,k2:list)->int:
+def compare(k1:List,k2:List)->int:
     d1=k1[0]*k1[0]+k1[1]*k1[1]
     d2=k2[0]*k2[0]+k2[1]*k2[1]
     if(d1<d2):
