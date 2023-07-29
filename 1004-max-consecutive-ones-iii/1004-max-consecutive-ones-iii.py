81f977d3b2c32e7a10aa05ee37b23102b15dec0a
GANGIPAMULA VENKATA SURYA AJAY <43990079+Ajay-Gangipamula@users.noreply.github.com>
2023-07-29 19:43:00 +0530

    Time: 678 ms (9.63%), Space: 17.4 MB (8.53%) - LeetHub

---
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -0,0 +1,57 @@
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        freq=[0]*2
+        countofzeros=0
+        end=0
+        countofones=0
+        maxi1=0
+        tempcount=0
+        if k==0:
+            for i in range(len(nums)):
+                if(nums[i]==1):
+                    tempcount+=1
+                else:
+                    tempcount=0
+                maxi1=max(maxi1,tempcount)
+                print(i,tempcount,maxi1,nums,len(nums))
+            return maxi1
+        for i in range(len(nums)):
+            freq[nums[i]]+=1
+
+        for i in range(len(nums)):
+            if(nums[i]==0):
+                countofzeros+=1
+                if(countofzeros==k):
+                    print(countofones)
+                    end=i
+                    break
+            else:
+                countofones+=1
+            if(i==len(nums)-1):
+                end=i
+                break
+        if(end==0 and nums[0]==0):
+            countofzeros=1
+
+        if(freq[0]==len(nums) and k==0):
+            return 0
+
+        start=0
+        maxm=end-start+1
+        print('hello',start,end,maxm,countofzeros,countofones)
+        while(end<len(nums)):
+            while(countofzeros > k):
+                if(nums[start]==0):
+                    countofzeros-=1
+                start+=1
+
+            if(end-start+1 > maxm):
+                maxm=end-start+1
+
+            end+=1
+            if(end<len(nums)):
+                if(nums[end]==0):
+                    countofzeros+=1
+            print('hai',start,end,countofzeros,maxm)
+            
+        return maxm
