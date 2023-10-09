b39f5753c061a007c82a6751546aaa7c241d85c5
GANGIPAMULA VENKATA SURYA AJAY <43990079+Ajay-Gangipamula@users.noreply.github.com>
2023-10-09 12:58:12 +0530

    Time: 1006 ms (5.41%), Space: 30 MB (87.89%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -0,0 +1,28 @@
+from collections import deque
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        stk=deque()
+        nse=[0]*len(heights)
+        for i in range(len(heights)):
+            while(len(stk)!=0 and heights[i]<heights[stk[-1]]):
+                nse[stk[-1]]=i
+                stk.pop()
+            stk.append(i)
+        while(len(stk)!=0):
+            nse[stk[-1]]=len(heights)
+            stk.pop()
+        print(nse)
+        pse=[0]*len(heights)
+        for i in range(len(heights)-1,-1,-1):
+            while(len(stk)!=0 and heights[i]<heights[stk[-1]]):
+                pse[stk[-1]]=i
+                stk.pop()
+            stk.append(i)
+        while(len(stk)!=0):
+            pse[stk[-1]]=-1
+            stk.pop()
+        print(pse)
+        maxarea=0
+        for i in range(len(heights)):
+            maxarea=max((nse[i]-pse[i]-1)*heights[i], maxarea)
+        return maxarea
