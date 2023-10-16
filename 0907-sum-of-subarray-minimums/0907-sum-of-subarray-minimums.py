9b78124a2fa6ca7383a693df686080220e1fd54a
GANGIPAMULA VENKATA SURYA AJAY <43990079+Ajay-Gangipamula@users.noreply.github.com>
2023-10-16 21:57:05 +0530

    Time: 373 ms (81.66%), Space: 20.8 MB (85.75%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
@@ -1,35 +1,12 @@
 from collections import deque
 class Solution:
-    def sumSubarrayMins(self, arr: List[int]) -> int:
-        m=10**9 +7
-        n=len(arr)
-        nse=[0]*n
-        psoe=[0]*n
-        stk=deque()
-        stk.append(0)
-        for i in range(1,n):
-            while(len(stk)!=0 and arr[i]<arr[stk[-1]]):
-                nse[stk[-1]]=i
-                stk.pop()
-            stk.append(i)
-        while(len(stk)!=0):
-            nse[stk[-1]]=n
-            stk.pop()
-
-        stk.append(n-1)
-        for i in range(n-2,-1,-1):
-            while(len(stk)!=0 and arr[i]<=arr[stk[-1]]):
-                psoe[stk[-1]]=i
-                stk.pop()
-            stk.append(i)
-        while(len(stk)!=0):
-            psoe[stk[-1]]=-1
-            stk.pop()
-
-        sum1=0
-        for i in range(n):
-            x1=i-psoe[i]
-            x2=nse[i]-i
-            x= (((x1%m * x2%m)%m) * (arr[i]%m))%m
-            sum1=(sum1%m + x%m)%m
-        return sum1
+    def sumSubarrayMins(self, A: List[int]) -> int:
+        res = 0
+        stack = []  #  non-decreasing 
+        A = [float('-inf')] + A + [float('-inf')]
+        for i, n in enumerate(A):
+            while stack and A[stack[-1]] > n:
+                cur = stack.pop()
+                res += A[cur] * (i - cur) * (cur - stack[-1]) 
+            stack.append(i)
+        return res % (10**9 + 7)
