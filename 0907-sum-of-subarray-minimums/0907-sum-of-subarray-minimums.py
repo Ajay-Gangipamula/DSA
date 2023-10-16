2c28ec2a1c675d15914458475cb150fb865bfac6
GANGIPAMULA VENKATA SURYA AJAY <43990079+Ajay-Gangipamula@users.noreply.github.com>
2023-10-16 21:41:51 +0530

    Time: 502 ms (6.44%), Space: 21 MB (26.64%) - LeetHub

---
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
@@ -0,0 +1,34 @@
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        m=10**9 +7
+        n=len(arr)
+        nse=[0]*n
+        psoe=[0]*n
+        stk=deque()
+        stk.append(0)
+        for i in range(1,n):
+            while(len(stk)!=0 and arr[i]<arr[stk[-1]]):
+                nse[stk[-1]]=i
+                stk.pop()
+            stk.append(i)
+        while(len(stk)!=0):
+            nse[stk[-1]]=n
+            stk.pop()
+
+        stk.append(n-1)
+        for i in range(n-2,-1,-1):
+            while(len(stk)!=0 and arr[i]<=arr[stk[-1]]):
+                psoe[stk[-1]]=i
+                stk.pop()
+            stk.append(i)
+        while(len(stk)!=0):
+            psoe[stk[-1]]=-1
+            stk.pop()
+
+        sum1=0
+        for i in range(n):
+            x1=(i-psoe[i])
+            x2=nse[i]-i
+            x= (((x1%m * x2%m)%m) * (arr[i]%m))%m
+            sum1=(sum1%m + x%m)%m
+        return sum1
