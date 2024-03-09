a2e15ddb7e62b8a14c1e09604ccd8bf8062f6194
GANGIPAMULA VENKATA SURYA AJAY <43990079+Ajay-Gangipamula@users.noreply.github.com>
2024-03-09 20:04:50 +0530

    Time: 40 ms (46.59%), Space: 16.5 MB (96.54%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -3,33 +3,24 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+def merge(list1,list2):
+    if(not list1):
+        return list2
+    if(not list2):
+        return list1
+    if(list1.val < list2.val):
+        head=list1
+        temp=list1.next
+        head.next=None
+        head.next=merge(temp,list2)
+    else:
+        head=list2
+        temp=list2.next
+        head.next=None
+        head.next=merge(list1,temp)
+    return head
+
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if(not list1):
-            return list2
-        if(not list2):
-            return list1
-        p1=list1
-        p2=list2
-        if(list1.val < list2.val):
-            head=list1
-            tail=list1
-            p1=p1.next
-        else:
-            head=list2
-            tail=list2
-            p2=p2.next
-        while(p1 and p2):
-            if(p1.val < p2.val):
-                tail.next=p1
-                p1=p1.next
-            else:
-                tail.next=p2
-                p2=p2.next
-            tail=tail.next
-        if(p1):
-            tail.next=p1
-        if(p2):
-            tail.next=p2
-        return head
-        
+        return merge(list1,list2)
