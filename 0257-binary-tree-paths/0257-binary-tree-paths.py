c40fc8423b38868a2ddd1070af85d55e18a3b4f4
GANGIPAMULA VENKATA SURYA AJAY <43990079+Ajay-Gangipamula@users.noreply.github.com>
2024-03-21 16:20:51 +0530

    Time: 39 ms (42.18%), Space: 16.5 MB (90.37%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.py
+++ 0257-binary-tree-paths/0257-binary-tree-paths.py
@@ -15,7 +15,6 @@ def roottoleaf(root,tmp,ans):
         tmp+="->"+str(root.val)
     if((not root.left) and (not root.right)):
         ans.append(tmp)
-    print(tmp)
     roottoleaf(root.left,tmp,ans)
     roottoleaf(root.right,tmp,ans)
 
