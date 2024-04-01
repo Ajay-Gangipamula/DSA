c245b0ff891dc4c3f2c64ec43be8eea0a00c1914
GANGIPAMULA VENKATA SURYA AJAY <43990079+Ajay-Gangipamula@users.noreply.github.com>
2024-04-01 16:40:05 +0530

    Time: 36 ms (49.19%), Space: 16.6 MB (27.65%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
@@ -5,15 +5,27 @@
 #         self.left = left
 #         self.right = right
 
-def postorder(root,l):
-    if(not root):
-        return
-    postorder(root.left,l)
-    postorder(root.right,l)
-    l.append(root.val)
+def postorder(root):
+    stk=deque()
+    stk.append((root,0))
+    l=[]
+    while(len(stk)!=0):
+        r=stk[-1]
+        stk.pop()
+        if(r[1]==1):
+            l.append(r[0].val)
+        else:
+            if(r[0]):
+                stk.append((r[0],1))
+                if(r[0].right):
+                    stk.append((r[0].right,0))
+                if(r[0].left):
+                    stk.append((r[0].left,0))
+    return l
+
+
 
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        l=[]
-        postorder(root,l)
+        l=postorder(root)
         return l
